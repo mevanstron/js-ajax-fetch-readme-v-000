16759a650b064a455acf886247ad8f8065a586e8
mevanstron <michael@michaelgregoryevans.com>
2018-04-03 17:06:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const app = "I don't do much.";
+const token = '80361dc6637ad8acf0c05b4b315b3b7b03d0d6b7'
+fetch('https://api.com/user/repos', {
+  headers: {
+    Authorization: `token $token`
+  }
+}).then(res => res.json()).then(json => console.log(json));
